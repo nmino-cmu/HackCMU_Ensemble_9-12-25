7f6ff5e90362b4621fe3d5a524f0f9d9a0c039e4
nmino-cmu <149437690+nmino-cmu@users.noreply.github.com>
2025-09-13 01:49:35 -0400

    Added venv

--- scripts/sound_to_wav.py
+++ scripts/sound_to_wav.py
@@ -1,1 +1,28 @@
 import ffmpeg
+import os
+from pathlib import Path
+
+
+
+def main():
+    HERE = Path(__file__).resolve().parent
+    SRC  = HERE.parent / "sound_data" / "data_raw"
+
+    for p in SRC.iterdir():
+        if p.is_file():
+            print("Input:", p.resolve())
+
+        
+    input_folder = "data_raw"
+    output_folder = "data_wav"
+    # abs_path = os.path.abspath(Path);
+
+    (
+    ffmpeg.input()
+    .output('output.wav', acodec="pcm_s32le", ar=96000, ac = 1)
+    .run()
+    )
+
+
+if __name__ == "__main__":
+    main()
