f18779457e80ad673fd52121b5346ae47f93b48d
MichaelHuynh13 <michaelhnanh@gmail.com>
2025-09-13 13:11:53 -0400

    frontend sans server

--- ensemble/src/App.jsx
+++ ensemble/src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './components/glare-hover.css';
+import GlareHover from './components/glare-hover.jsx';
+import folderImg from "./assets/folder.png";
+import folderOpenImg from "./assets/openedfolder.png"
+import CircularText from './CircularText';
+
+
+function setFolderOpen() {
+  document.getElementById("openedFolder").src = {folderOpenImg};
+}
+
+function setFolderClose() {
+  document.getElementById("Folder").src = {folderImg};
+}
 
 function App() { 
-  const [count, setCount] = useState(0)
-
+  const [hover, setHover] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <GlareHover>
+        <input
+        id="fileInput"
+        type="file"
+        style={{ display: "none" }}
+        onChange={(e) => console.log(e.target.files)}
+        />
+        <label htmlFor='fileInput'>
+          <img src ={folderImg} alt = "open your file" width={150}/>
+        </label>
+      </GlareHover>
+
+      
+    </div>
+  );
 }
 
 export default App
